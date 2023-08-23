b88f990fdd18b6227db76e2eae26dd5616847c50
GAOFENGBO <125358092+kouhouhaku@users.noreply.github.com>
2023-08-23 16:07:31 +0900

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,3 +1,33 @@
-;(() => {
+; (() => {
   // 请在这里完成代码
+  const first = document.querySelector('#first')
+  const second = document.querySelector('#second')
+  const operators = document.querySelectorAll('.operators input')
+  const submit = document.querySelector('button')
+  const result = document.querySelector('#result')
+
+  function calculate() {
+    if (operators[0].checked) {
+      return Number(first.value) + Number(second.value)
+    } else if (operators[1].checked) {
+      return Number(first.value) - Number(second.value)
+    } else if (operators[2].checked) {
+      return Number(first.value) * Number(second.value)
+    } else if (operators[3].checked) {
+      if (second.value === '0') {
+        return NaN
+      } else {
+        return Number(first.value) / Number(second.value)
+      }
+    }
+  }
+
+  function clickSubmit() {
+    result.innerHTML = `结果：<br> ${calculate()}`
+  }
+
+  submit.addEventListener('click', function (e) {
+    e.preventDefault()
+  })
+  submit.addEventListener('click', clickSubmit)
 })()
